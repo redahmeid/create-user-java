6333fc2fd9f0d355a70a6b953c9ef03731501643
redahmeid <reda@redahmeid.com>
2017-10-26 16:08:41 +0100

    input - 2

--- src/main/java/io/openlight/lambda/CreateUserHandler.java
+++ src/main/java/io/openlight/lambda/CreateUserHandler.java
@@ -12,15 +12,15 @@ import java.util.HashMap;
 import java.util.Map;
 
 
-public class CreateUserHandler implements RequestHandler<User, Object> {
+public class CreateUserHandler implements RequestHandler<Object, Object> {
 
     Gson gson = new Gson();
 
-    public GatewayResponse handleRequest(final User input, final Context context) {
+    public GatewayResponse handleRequest(final Object input, final Context context) {
         Map<String, String> headers = new HashMap<>();
         headers.put("Content-Type", "application/json");
         System.out.println(gson.toJson(input));
-        String id = Inserter.insert(input.name,input.email);
+        String id = Inserter.insert("Hello","Hi");
         Link link = new Link();
         link.location = "http://api.openlight.io/users/"+id;
         String linkJson = gson.toJson(link);
