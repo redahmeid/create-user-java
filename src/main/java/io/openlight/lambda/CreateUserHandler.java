e30b801a98ff7e52b8d948bc03f632d1044682da
redahmeid <reda@redahmeid.com>
2017-10-26 13:33:25 +0100

    first step towards api

--- src/main/java/io/openlight/lambda/CreateUserHandler.java
+++ src/main/java/io/openlight/lambda/CreateUserHandler.java
@@ -2,19 +2,29 @@ package io.openlight.lambda;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.google.gson.Gson;
+
+import io.openlight.domain.User;
+import io.openlight.response.Link;
+import io.openlight.response.UserResponse;
 
 import java.util.HashMap;
 import java.util.Map;
 
 
-/**
- * Handler for requests to Lambda function.
- */
 public class CreateUserHandler implements RequestHandler<Object, Object> {
+
+    Gson gson = new Gson();
 
     public Object handleRequest(final Object input, final Context context) {
         Map<String, String> headers = new HashMap<>();
         headers.put("Content-Type", "application/json");
-        return new GatewayResponse("{ \"Output\": \"Hello World!\"}", headers, 200);
+
+        Link link = new Link();
+        link.location = "http://api.openlight.io/users/1234";
+        String linkJson = gson.toJson(link);
+        
+
+        return new GatewayResponse(linkJson,headers, 201);
     }
 }
