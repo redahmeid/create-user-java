2e830945bed76f493238a7bc6f1200776a61edb4
redahmeid <reda@redahmeid.com>
2017-10-26 15:35:31 +0100

    input

--- src/main/java/io/openlight/lambda/CreateUserHandler.java
+++ src/main/java/io/openlight/lambda/CreateUserHandler.java
@@ -19,7 +19,7 @@ public class CreateUserHandler implements RequestHandler<User, Object> {
     public GatewayResponse handleRequest(final User input, final Context context) {
         Map<String, String> headers = new HashMap<>();
         headers.put("Content-Type", "application/json");
-
+        System.out.println(input);
         String id = Inserter.insert(input.name,input.email);
         Link link = new Link();
         link.location = "http://api.openlight.io/users/"+id;
